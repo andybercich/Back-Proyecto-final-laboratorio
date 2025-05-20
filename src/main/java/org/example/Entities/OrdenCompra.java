582f40936fbb70649362c4f6de20a1f6f3802730
PabloLimaDelgado <limapablomdz@gmail.com>
2025-05-20 10:54:54 -0300

    Cambios OrdenCompraDetalle

--- src/main/java/org/example/Entities/OrdenCompra.java
+++ src/main/java/org/example/Entities/OrdenCompra.java
@@ -1,9 +1,6 @@
 package org.example.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,7 +20,9 @@ import java.time.LocalDate;
 @EqualsAndHashCode(callSuper = true)
 public class OrdenCompra extends Base{
 
-    @NotNull(message = "La orden compra debe estar relacionado con un usuario")
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    @NotNull(message = "La orden compra debe estar relacionada con un usuario")
     private Usuario usuario;
 
     private BigDecimal total;
@@ -32,6 +31,9 @@ public class OrdenCompra extends Base{
 
     private LocalDate fecha;
 
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "direccion_id", nullable = false)
+    @NotNull(message = "Debe especificar una dirección válida")
     private Direccion direccion;
 
     @NotNull(message = "Determina si se usará la direccion del usuario o no")
