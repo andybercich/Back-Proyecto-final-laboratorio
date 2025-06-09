9f1c142baa3bec3e106ce036ddc31440d677de2d
ANDRÉS BERCICH <andybercich6@gmail.com>
2025-06-09 20:58:09 -0300

    Arreglando direccion

--- src/main/java/org/example/Services/DireccionService.java
+++ src/main/java/org/example/Services/DireccionService.java
@@ -1,12 +1,24 @@
 package org.example.Services;
 
 import org.example.Entities.Direccion;
+import org.example.Entities.Usuario;
+import org.example.JWT.JwtService;
 import org.example.Repositories.DireccionRepository;
+import org.example.Repositories.UsuarioRepository;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DireccionService extends BaseService<Direccion, Long, DireccionRepository> {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     public Direccion update(Long id, Direccion entity) throws Exception {
         try {
@@ -22,4 +34,23 @@ public class DireccionService extends BaseService<Direccion, Long, DireccionRepo
             throw new Exception(e.getMessage());
         }
     }
+    public Direccion saveToken(Direccion entity) {
+        try {
+            Usuario user = usuarioRepository.findByMail(
+                    SecurityContextHolder.getContext().getAuthentication().getName()
+            ).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+            user.getDirecciones().add(entity);
+            entity.getUsuarios().add(user);
+            repository.save(entity);
+            usuarioRepository.save(user);
+
+
+            return entity;
+        } catch (Exception e) {
+            throw new RuntimeException("No se pudo crear la dirección: " + e.getMessage());
+        }
+    }
+
+
 }
--- src/main/java/org/example/Services/OrdenCompraService.java
+++ src/main/java/org/example/Services/OrdenCompraService.java
@@ -1,9 +1,12 @@
 package org.example.Services;
 
 import org.example.Entities.OrdenCompra;
+import org.example.Entities.OrdenCompraDetalle;
 import org.example.Repositories.OrdenCompraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrdenCompraService extends BaseService<OrdenCompra,Long, OrdenCompraRepository >{
+
 }
